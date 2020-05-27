55bb310b90688fe9f357cecaa8db5572bfb3f0cb
Abhishek Sharma <sharmaash876@gmail.com>
2020-05-27 20:05:09 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,19 +1,53 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React , { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+
+  const [addState , setAddState] = useState('')
+  const [list , setList] = useState([])
+
+  const addStateHandler = (event) => {
+    setAddState(event)
+  }
+
+  const listHandler = () => {
+    setList(handler => [...list , addState])
+  }
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput 
+         placeholder="Course Goal" 
+         style={styles.input}
+         onChangeText={addStateHandler}
+         value={addState}
+         />
+        <Button 
+        title="ADD"
+        onPress={listHandler}
+        />
+      </View>
+      <View>
+  {list.map(goal => <Text key={goal}>{goal}</Text>)}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50
   },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  input: {
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10
+  }
 });
